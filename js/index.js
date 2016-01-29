fcba3ffec91e5a8772db6c3cdce7ba9417e47bb6
Pavel Ermakov <pavermakov@gmail.com>
2016-01-29 11:50:28 +0300

    minor changes

--- js/index.js
+++ js/index.js
@@ -1,48 +1,46 @@
-var MAX_LETTERS = 20;
-var $tasks = $('#tasks');
-var $text = $('#newTask').find('input[type="text"]');
-var $button = $('input[type="button"]');
-var $counter = $('#counter');
+$(document).ready(function(){
+  var MAX_LETTERS = 20;
+  var $tasks = $('#tasks');
+  var $text = $('#newTask').find('input[type="text"]');
+  var $button = $('input[type="button"]');
+  var $counter = $('#counter');
 
-$button.on('click', addTask);
+  $button.on('click', addTask);
+  $tasks.on('click', 'span', removeTask);
+  $text.on('focus', toggleCounter);
+  $text.on('blur', toggleCounter);
+  $text.on('keyup', countLetters);
 
-$tasks.on('click', 'span', removeTask);
-
-$text.on('focus', toggleCounter);
-
-$text.on('blur', toggleCounter);
-
-$text.on('keyup', countLetters);
-
-function addTask() {
-  if ($.trim($text.val()) !== '') {
-    var $task = $('<li>' + $text.val() + '<span><i class="fa fa-times"></i></span></li>');
-    addAnimation($task, 'fadeIn');
-    $task.appendTo($tasks);
-    reset();
+  function addTask() {
+    if ($.trim($text.val()) !== '') {
+      var $task = $('<li>' + $text.val() + '<span><i class="fa fa-times"></i></span></li>');
+      addAnimation($task, 'fadeIn');
+      $task.appendTo($tasks);
+      reset();
+    }
   }
-}
 
-function removeTask() {
-  $(this).closest('li').remove();
-}
-
-function toggleCounter() {
-  $counter.closest('p').toggle();
-}
-
-function countLetters() {
-  var $letters = MAX_LETTERS - $text.val().length;
-  if ($letters >= 0) {
-    $counter.html($letters);
+  function removeTask() {
+    $(this).closest('li').remove();
   }
-}
 
-function addAnimation(element, animation){
-  element.addClass('animated ' + animation);
-}
+  function toggleCounter() {
+    $counter.closest('p').toggle();
+  }
 
-function reset() {
-  $text.val('');
-  $counter.html(MAX_LETTERS);
-}
+  function countLetters() {
+    var $letters = MAX_LETTERS - $text.val().length;
+    if ($letters >= 0) {
+      $counter.html($letters);
+    }
+  }
+
+  function addAnimation(element, animation){
+    element.addClass('animated ' + animation);
+  }
+
+  function reset() {
+    $text.val('');
+    $counter.html(MAX_LETTERS);
+  }
+});
